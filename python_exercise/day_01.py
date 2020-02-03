0589e48e1cdc1fea2b40d19512e6051f646d2e0b
liujunkong <1196689756@qq.com>
2020-02-03 16:16:37 +0800

    day_01 exercise end

--- python_exercise/day_01.py
+++ python_exercise/day_01.py
@@ -2,4 +2,12 @@
 from PIL import Image,ImageDraw,ImageFont
 import os
 
+len=200#字体大小
+myfont=ImageFont.truetype("arial.ttf",len) #选择字体和大小。
+photo=Image.open("wechat.jpg") #选择图片
+add_num=ImageDraw.Draw(photo)
+fillcolor="red"#字体颜色
+(width,height)=photo.size
+add_num.text((width-len/2,0),u"4",font=myfont,fill=fillcolor)#第一个参数为添加文字的坐标
+photo.save("new.jpg","jpeg")
 
