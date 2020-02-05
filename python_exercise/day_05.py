55a69bf65c26c7c60995a8372c1cb5569d72cd6b
liujunkong <1196689756@qq.com>
2020-02-05 18:55:12 +0800

    day_05 end

--- python_exercise/day_05.py
+++ python_exercise/day_05.py
@@ -1,1 +1,11 @@
-print("111")
+import os
+from PIL import Image
+filepath="D:\\photo\\"
+filename=os.listdir(filepath)
+for i in range(len(filename)):
+    ima=Image.open(filepath+"%s"%(filename[i]))
+    ima_new=ima.resize((640,480))
+    ima_new.save(filepath+"new_%s.png" % (i))#生成图片保存到原有路径
+
+
+
