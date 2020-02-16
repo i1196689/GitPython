4e9123bab5022fd011993db4b3a2393dad541815
liujunkong <1196689756@qq.com>
2020-02-16 20:27:38 +0800

    update

---
+++ python_lessons/day_13.py
@@ -0,0 +1,27 @@
+'''
+s1="hello word"
+print(s1[0])
+print(s1[2])
+
+print(s1[6:9])
+print(s1[6:])
+print(s1[::-1])
+print(s1[::2])
+
+print(10*s1)
+
+print("b" in s1)
+print("h" in s1)
+print(len(s1))
+for i in s1:
+    print(i)
+
+print(min(s1),max(s1))
+a=[1,2,3,0,6]
+print(2 in a)
+print(min(a),max(a))
+print(10*a)
+a[0]=20
+
+print(min(a),max(a),sep="\n")
+'''
