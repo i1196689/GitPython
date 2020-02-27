3f455d0901efb23e606ea20084cf6a109e9e7d76
liujunkong <1196689756@qq.com>
2020-02-27 21:05:11 +0800

    update

---
+++ python_xiaojiayu/day_03.py
@@ -0,0 +1,8 @@
+str1='listening practice'
+print(str1[:6]+'  插入字符串  '+str1[6:])
+print(dir(str))
+str1=str1.capitalize()
+print(str1)
+print(str1.center(50))
+print(str1.endswith('ce'))
+print(str1.find('st'))
